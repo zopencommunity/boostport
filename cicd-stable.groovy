6ea9d0910c32ec1763c27b61f515caca7d838baf
Haritha <112700353+HarithaIBM@users.noreply.github.com>
2024-07-10 10:26:52 +0530

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/boostport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/boostport.git'), string(name: 'PORT_DESCRIPTION', value: 'The project aims at generating [D[D[D[D[D[D[D[D[D[D[Dporting boost librai[Dries to Z operating system' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/boostport.git'), string(name: 'PORT_DESCRIPTION', value: 'The project aims at generating boost libraries to Z operating system' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1") ]
   }
 }
