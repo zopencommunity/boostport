2cdaa95944e8246066b8b28f19e7445345baadad
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:10 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/boostport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/boostport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/boostport.git'), string(name: 'PORT_DESCRIPTION', value: 'The project aims at generating boost libraries to Z operating system' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1") ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/boostport.git'), string(name: 'PORT_DESCRIPTION', value: 'The project aims at generating boost libraries to Z operating system' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1") ]
   }
 }
